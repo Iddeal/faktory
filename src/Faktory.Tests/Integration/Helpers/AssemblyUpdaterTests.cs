50a216d13acb0c70443560d58dad6e3968cef462
Michael Minton <michael@minton.io>
2024-01-25 17:59:08 -0500

    Introduce AssemblyUpdater This allows setting attributes in a C# AssemblyInfo.cs file

---
+++ src/Faktory.Tests/Integration/Helpers/AssemblyUpdaterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Faktory.Core.Helpers;
+using NUnit.Framework;
+
+namespace Faktory.Tests.Integration.Helpers
+{
+    [TestFixture]
+    [NonParallelizable]
+    [Category("Integration")]
+    public class AssemblyUpdaterTests
+    {
+        const string assemblyInfoPath = @"..\..\..\..\MsBuildTest\Properties\AssemblyInfo.cs";
+
+        [Test, Order(1)]
+        [NonParallelizable]
+        public void Update_NonExistentFile_Fails()
+        {
+            Assert.Throws<Exception>(() => AssemblyUpdater.Update("I-dont-exist.nada", AssemblyUpdater.AssemblyInfo.Default()));
+        }
+
+        [Test, Order(2)]
+        [NonParallelizable]
+        public void Update_ValidFile_Succeeds()
+        {
+
+            // Arrange 
+            var before = File.ReadAllText(assemblyInfoPath);
+            var attributes = new AssemblyUpdater.AssemblyInfo()
+            {
+                Title = "My Title",
+                Description = "My Description",
+                Company = "My Company",
+                Product = "My Product",
+                Copyright = "My Copyright",
+                Trademark = "My Trademark",
+                Version = "My Version",
+                FileVersion = "My FileVersion"
+            };
+
+            // Act - Clean the path
+            AssemblyUpdater.Update(assemblyInfoPath, attributes);
+            var after = File.ReadAllText(assemblyInfoPath);
+
+            // Reset the original file
+            File.WriteAllText(assemblyInfoPath, before);
+
+            // Assert Before State
+            StringAssert.Contains("AssemblyTitle(\"MsBuildTest\")", before);
+            StringAssert.Contains("AssemblyDescription(\"\")", before);
+            StringAssert.Contains("AssemblyConfiguration(\"\")", before);
+            StringAssert.Contains("AssemblyCompany(\"\")", before);
+            StringAssert.Contains("AssemblyProduct(\"MsBuildTest\")", before);
+            StringAssert.Contains("AssemblyCopyright(\"Copyright ©  2024\")", before);
+            StringAssert.Contains("AssemblyTrademark(\"\")", before);
+            StringAssert.Contains("AssemblyVersion(\"1.0.0.0\")", before);
+            StringAssert.Contains("AssemblyFileVersion(\"1.0.0.0\")", before);
+
+            // Assert After State
+            StringAssert.Contains("AssemblyTitle(\"My Title\")", after);
+            StringAssert.Contains("AssemblyDescription(\"My Description\")", after);
+            StringAssert.Contains("AssemblyConfiguration(\"\")", after);
+            StringAssert.Contains("AssemblyCompany(\"My Company\")", after);
+            StringAssert.Contains("AssemblyProduct(\"My Product\")", after);
+            StringAssert.Contains("AssemblyCopyright(\"My Copyright\")", after);
+            StringAssert.Contains("AssemblyTrademark(\"My Trademark\")", after);
+            StringAssert.Contains("AssemblyVersion(\"My Version\")", after);
+            StringAssert.Contains("AssemblyFileVersion(\"My FileVersion\")", after);
+        }
+    }
+}
---
+++ src/Faktory/Helpers/AssemblyUpdater.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Faktory.Core.Helpers;
+
+public class AssemblyUpdater
+{
+    public struct AssemblyInfo{
+        public string Title {get;set;}
+        public string Description { get; set; }
+        public string Company { get; set; }
+        public string Product { get; set; }
+        public string Copyright { get; set; }
+        public string Trademark { get; set; }
+        public string Version { get; set; }
+        public string FileVersion { get; set; }
+
+        public static AssemblyInfo Default()
+        {
+            return new AssemblyInfo();
+        }
+    }
+    
+    public static void Update(string filePath, AssemblyInfo attributes){
+        if (File.Exists(filePath) == false) { throw new Exception($"AssemblyInfo not found at '{filePath}'"); }
+
+        var originalFile = File.ReadAllText(filePath);
+        File.WriteAllText(filePath, UpdateAttributes(originalFile, attributes));
+    }
+	
+    static string UpdateAttributes(string original, AssemblyInfo attributes) => GetProperties(attributes)
+        .Aggregate(original, (current, prop) => UpdateAttribute(current, prop.Key, prop.Value));
+
+    static Dictionary<string, string> GetProperties(AssemblyInfo attributes) =>
+        attributes.GetType().GetProperties()
+            .ToDictionary(prop => prop.Name, prop => (string)prop.GetValue(attributes, null));
+
+    static string UpdateAttribute(string original, string attribute, string value)
+    {
+        const RegexOptions options = RegexOptions.Multiline | RegexOptions.None;
+        value = $"\"{value}\""; // Need to add quotes around the value
+        var pattern = Pattern(attribute);
+        var m = Regex.Match(original, pattern, options);
+        switch (m.Success)
+        {
+            case true when m.Value != value:
+                Boot.Logger.Verbose($"Attribute[{attribute}] - Setting to {value}");
+                return Regex.Replace(original, pattern, value, options);
+            case true:
+                Boot.Logger.Verbose($"Attribute[{attribute}] - Value already equals {value}");
+                return original;
+            default:
+                throw new Exception($"Attribute[{attribute}] - Could not find attribute");
+        }
+    }
+
+    static string Pattern(string attribute) => $@"(?<=^\s*\[\s*assembly:\s*Assembly{attribute}(?:Attribute)?\s*\()(.*)(?=\)\s*\]\s*$)";
+}
+
