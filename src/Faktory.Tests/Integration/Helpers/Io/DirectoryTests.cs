0e67d2e9e69942c478f8f09c80d3d2153ca4f6e7
Michael Minton <michael@minton.io>
2024-01-29 11:33:49 -0500

    Introduce Io.Directory Helper

---
+++ src/Faktory.Tests/Integration/Helpers/Io/DirectoryTests.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Faktory.Tests.Integration.Helpers.Io
+{
+    [TestFixture]
+    [NonParallelizable]
+    [Category("Integration")]
+    public class DirectoryTests
+    {
+        const string BasePath = "./GlobbingTests";
+
+        [SetUp]
+        public void ResetDestination()
+        {
+            TestHelpers.Disk.Reset(BasePath);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            TestHelpers.Disk.Reset(BasePath);
+        }
+
+        [Test, Order(1)]
+        [NonParallelizable]
+        public void GetAllFilesMatching_NestedFoldersEachWithJpegs_ReturnsEveryJpegFromAllNestedFolders()
+        {
+            // Arrange 
+            TestHelpers.Disk.CreateFile(BasePath, "I_am_a_base.dll");
+            TestHelpers.Disk.CreateFile(BasePath, "I_am_a_base.txt");
+            TestHelpers.Disk.CreateFile(BasePath, "I_am_a_base.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderA"), "I_am_a_FolderA.png");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderA"), "I_am_a_FolderA.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderA"), "FolderA_123.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderA"), "FolderA_456.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderA"), "FolderA_789.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderB"), "FolderB_B12.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderC"), "FolderC_C12.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(BasePath, "FolderD"), "FolderD_D12.jpg");
+            TestHelpers.Disk.CreateFile(Path.Combine(Path.Combine(BasePath, "FolderD"), "ThreeDeepFolderA"), "ThreeDeepFolderA_ABC.jpg");
+
+            // Act - Clean the path
+            var files = Core.Helpers.Io.GetAllFilesMatching(BasePath, "*.jpg").ToList();
+
+            // Assert
+            CollectionAssert.Contains(files, @"./GlobbingTests\I_am_a_base.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderA\FolderA_123.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderA\FolderA_456.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderA\FolderA_789.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderA\I_am_a_FolderA.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderB\FolderB_B12.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderC\FolderC_C12.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderD\FolderD_D12.jpg");
+            CollectionAssert.Contains(files, @"./GlobbingTests\FolderD\ThreeDeepFolderA\ThreeDeepFolderA_ABC.jpg");
+            Assert.That(files.Count(), Is.EqualTo(9));
+        }
+    }
+}
---
+++ src/Faktory/Helpers/IoDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Faktory.Core.Helpers;
+
+/// <summary>
+/// Set of helpers for working with Disk operations.
+/// </summary>
+public static partial class Io
+{
+    /// <summary>
+    /// Recursively search for matching files under the path including subfolders.
+    /// </summary>
+    /// <param name="path">Directory to search.</param>
+    /// <param name="pattern">The search string to match against the names of files in <paramref name="path" />.  This parameter can contain a combination of valid literal path and wildcard (* and ?) characters, but it doesn't support regular expressions.</param>
+    /// <returns></returns>
+    public static IEnumerable<string> GetAllFilesMatching(string path, string pattern)
+    {
+        foreach (var file in Directory.EnumerateFiles(path, pattern))
+        {
+            yield return file;
+        }
+
+        foreach (var d in Directory.EnumerateDirectories(path))
+        {
+            foreach (var file in GetAllFilesMatching(d, pattern))
+            {
+                yield return file;
+            }
+        }
+    }
+}
