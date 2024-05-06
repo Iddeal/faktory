9ac4610c994473afe0d46d4f5207c1a5ee15c3a0
Michael Minton <michael@minton.io>
2024-05-06 16:12:25 -0400

    Fix paths on Sample app for easier debugging

--- src/Faktory.Sample/Program.cs
+++ src/Faktory.Sample/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.IO;
 using System.Threading;
 using Faktory.Core;
 using Faktory.Core.Helpers;
@@ -13,18 +13,30 @@ namespace Faktory.Sample
         public static void Main(string[] args)
         {
             FaktoryProgram.Run(args);
+            Console.ReadLine();
         }
     }
 
     public class MyFaktory : Core.Faktory
     {
-        string _buildDir = SourcePath + @"src\build\";
-        string _stageDir = SourcePath + @"src\bin\";
-        string _solutionPath = SourcePath + @"src\MsBuildTest.sln";
+        string _rootDir;
+        string _buildDir;
+        string _stageDir;
+        string _solutionPath;
+
+        static string MsBuildPath => $@"C:\Program Files\Microsoft Visual Studio\2022\{VisualStudioEdition}\MSBuild\Current\Bin\amd64\MSBuild.exe";
+        static string VisualStudioEdition =>
+            Directory.Exists(@"C:\Program Files\Microsoft Visual Studio\2022\Professional")
+                ? "Professional"
+                : "Enterprise";
 
         protected override void Configure()
         {
-            Config.Set("MSBuildPath", @"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\amd64\MSBuild.exe");
+            Config.Set("MSBuildPath", MsBuildPath);
+            _rootDir = new DirectoryInfo(Path.Combine(SourcePath, @"..\..\..\..\..\")).FullName;
+            _buildDir = Path.Combine(_rootDir, @"src\build\");
+            _stageDir = Path.Combine(_rootDir, @"src\bin\");
+            _solutionPath = Path.Combine(_rootDir, @"src\MsBuildTest\MsBuildTest.sln");
         }
 
         protected override void RunBuild()
@@ -48,14 +60,14 @@ namespace Faktory.Sample
             Log("I am cleaning...");
             CleanDirectory(_buildDir);
             CleanDirectory(_stageDir);
-            MsBuild.Clean(_solutionPath, "Release", "Any CPU");
+            MsBuild.Clean(_solutionPath, "Release", "x64");
         }
 
         void Build()
         {
             Log("I am building...");
             Thread.Sleep(300);
-            MsBuild.Run(_solutionPath, "Release", "Any CPU");
+            MsBuild.Run(_solutionPath, "Release", "x64");
         }
 
         void Stage()
