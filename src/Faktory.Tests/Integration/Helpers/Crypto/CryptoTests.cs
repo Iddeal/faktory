127efe03c05b62f3e0e5e0bd0aca82fed848d26e
Michael Minton <michael@minton.io>
2025-01-20 17:34:42 -0500

    Introduce Crypto.GetFileHash helper

---
+++ src/Faktory.Tests/Integration/Helpers/Crypto/CryptoTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Faktory.Tests.Integration.Helpers.Crypto
+{
+    [TestFixture]
+    [NonParallelizable]
+    [Category("Integration")]
+    public class CryptoTests
+    {
+        [Test, Order(1)]
+        [NonParallelizable]
+        public void GetFileHash_NotFoundApp_ReturnsFailure()
+        {
+            // Act 
+            var exception = Assert.Throws<FileNotFoundException>(() => Core.Helpers.Crypto.GetFileHash("nada_i_dont_exist.exe"));
+
+            // Assert
+            StringAssert.Contains("Could not find file", exception.Message);
+        }
+
+        [Test, Order(2)]
+        [NonParallelizable]
+        public void GetFileHash_OnExe_ReturnsHash()
+        {
+            // Arrange
+            const string knownHash = "DE6478C25D3DEEC68ADA12CDA8D05B79C4526FB175304BE3B82557ACC1CD400B";
+            // Act 
+            var hash = Core.Helpers.Crypto.GetFileHash("dummy.exe");
+
+            // Assert
+            Assert.AreEqual(knownHash, hash);
+        }
+    }
+}
---
+++ src/Faktory/Helpers/Crypto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Faktory.Core.Helpers;
+
+public static class Crypto
+{
+    public static string GetFileHash(string filePath)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            using (var stream = File.OpenRead(filePath))
+            {
+                var hashBytes = sha256.ComputeHash(stream);
+                return BitConverter
+                    .ToString(hashBytes)
+                    .Replace("-", "")
+                    .ToUpperInvariant();
+            }
+        }
+    }
+}
